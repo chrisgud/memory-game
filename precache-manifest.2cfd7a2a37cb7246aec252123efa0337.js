1cd6530aa0d2ddc332d31c6ac8e30b63e243f0a7
Chris Gudmundsson <chrisgud85@gmail.com>
2018-11-17 12:46:04 -0600

    Updates

---
+++ precache-manifest.2cfd7a2a37cb7246aec252123efa0337.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = [
+  {
+    "revision": "66c34b29d278a5d14e76",
+    "url": "/memory-game/static/js/runtime~main.66c34b29.js"
+  },
+  {
+    "revision": "70cb2b659539b980b27c",
+    "url": "/memory-game/static/js/main.70cb2b65.chunk.js"
+  },
+  {
+    "revision": "9979aa7a1677ea71fa5b",
+    "url": "/memory-game/static/js/1.9979aa7a.chunk.js"
+  },
+  {
+    "revision": "70cb2b659539b980b27c",
+    "url": "/memory-game/static/css/main.23bae8f6.chunk.css"
+  },
+  {
+    "revision": "9979aa7a1677ea71fa5b",
+    "url": "/memory-game/static/css/1.656758f2.chunk.css"
+  },
+  {
+    "revision": "c2ff24743f2720d5598cc558bd48bef8",
+    "url": "/memory-game/index.html"
+  }
+];
